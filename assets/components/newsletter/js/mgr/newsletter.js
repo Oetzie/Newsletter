93bb7f1ed2ee8b6e1ca9acf54f46ae5d7f46a920
Oetzie <info@oetzie.nl>
2016-12-15 12:46:03 +0100

    Added

---
+++ assets/components/newsletter/js/mgr/newsletter.js
@@ -0,0 +1,19 @@
+var Newsletter = function(config) {
+	config = config || {};
+	
+	Newsletter.superclass.constructor.call(this, config);
+};
+
+Ext.extend(Newsletter, Ext.Component, {
+	page	: {},
+	window	: {},
+	grid	: {},
+	tree	: {},
+	panel	: {},
+	combo	: {},
+	config	: {}
+});
+
+Ext.reg('newsletter', Newsletter);
+
+Newsletter = new Newsletter();
---
+++ assets/components/newsletter/js/mgr/widgets/home.panel.js
@@ -0,0 +1,84 @@
+Newsletter.panel.Home = function(config) {
+	config = config || {};
+	
+    Ext.apply(config, {
+		id			: 'newsletter-panel-home',
+		cls			: 'container',
+		defaults	: {
+			collapsible	: false,
+			autoHeight	: true,
+			autoWidth	: true,
+			border		: false
+		},
+		items		: [{
+			html		: '<h2>'+_('newsletter')+'</h2>',
+			id			: 'newsletter-header',
+			cls			: 'modx-page-header'
+		}, {
+			xtype		: 'modx-tabs',
+			items		: [{
+				layout		: 'form',
+				title		: _('newsletter.newsletters'),
+				defaults	: {
+					autoHeight	: true,
+					autoWidth	: true,
+					border		: false
+				},
+				items		: [{
+					html			: '<p>'+_('newsletter.newsletters_desc')+'</p>',
+					bodyCssClass	: 'panel-desc'
+				}, {
+		            html			: Newsletter.config.admin && 0 == parseInt(MODx.config['newsletter.cronjob']) ? '<p>' + _('newsletter.newsletter_cronjob_notice_desc') + '</p>' : '',
+					bodyCssClass	: Newsletter.config.admin && 0 == parseInt(MODx.config['newsletter.cronjob']) ? 'modx-config-error' : ''
+	            }, {
+		            html			: 0 == parseInt(MODx.config.site_status) ? '<p>' + _('newsletter.newsletter_site_status_notice_desc') + '</p>' : '',
+					bodyCssClass	: 0 == parseInt(MODx.config.site_status) ? 'modx-config-error' : ''
+	            }, {
+					xtype			: 'newsletter-grid-newsletters',
+					cls				: 'main-wrapper',
+					preventRender	: true
+				}]
+			}, {
+				layout		: 'form',
+				title		: _('newsletter.subscriptions'),
+				defaults	: {
+					autoHeight	: true,
+					autoWidth	: true,
+					border		: false
+				},
+				items		: [{
+					html			: '<p>'+_('newsletter.subscriptions_desc')+'</p>',
+					bodyCssClass	: 'panel-desc'
+				}, {
+					xtype			: 'newsletter-grid-subscriptions',
+					cls				: 'main-wrapper',
+					refreshCmp		: 'newsletter-grid-lists',
+					preventRender	: true
+				}]
+			}, {
+				layout		: 'form',
+				title		: _('newsletter.lists'),
+				defaults	: {
+					autoHeight	: true,
+					autoWidth	: true,
+					border		: false
+				},
+				items		: [{
+					html			: '<p>'+_('newsletter.lists_desc')+'</p>',
+					bodyCssClass	: 'panel-desc'
+				}, {
+					xtype			: 'newsletter-grid-lists',
+					cls				: 'main-wrapper',
+					refreshCmp 		: 'newsletter-grid-subscriptions',
+					preventRender	: true
+				}]
+			}]
+		}]
+	});
+
+	Newsletter.panel.Home.superclass.constructor.call(this, config);
+};
+
+Ext.extend(Newsletter.panel.Home, MODx.FormPanel);
+
+Ext.reg('newsletter-panel-home', Newsletter.panel.Home);
